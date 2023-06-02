b5ad58805457314825b5f438c1cc3fd807a071ba
rguidrylsua <rguidry@lsua.edu>
2023-06-02 08:42:05 -0500

    Added Abstract

--- Section15_Udemy/Section15_Udemy/Abstract/AbstractForm.Designer.cs
+++ Section15_Udemy/Section15_Udemy/Abstract/AbstractForm.Designer.cs
@@ -28,19 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnShow = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // btnShow
+            // 
+            this.btnShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShow.Location = new System.Drawing.Point(234, 12);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(109, 46);
+            this.btnShow.TabIndex = 0;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 264);
+            this.textBox1.TabIndex = 1;
             // 
             // AbstractForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(420, 309);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnShow);
             this.Name = "AbstractForm";
             this.Text = "Abstract";
+            this.Load += new System.EventHandler(this.AbstractForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
